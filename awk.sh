513bad58080260a705c70ab58954894bbf3d94e1
peng.1895 <85783400+wpf95@users.noreply.github.com>
2023-07-26 17:42:36 +0800

    Update awk.sh

--- awk.sh
+++ awk.sh
@@ -1,1 +1,1 @@
-less roh_island.pos | uniq -c | awk '{$1=$1/60;print}' | awk -v OFS='\t' '{print $2,$3,$1}' > roh_island_indicine
+less roh_island.pos |sort -k1,1n -k2,2n | uniq -c | awk '{$1=$1/60;print}' | awk -v OFS='\t' '{print $2,$3,$1}' > roh_island_indicine
